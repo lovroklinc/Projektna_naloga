7c1254bfcc252735c120404efd1e5b1060d4aa04
lovroklinc <lovro.klinc@gmail.com>
2024-08-20 10:31:32 +0200

    Popravil stopničasto zgradbo funkcij in dodal izdelavo slovarja.

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -14,27 +14,34 @@ def zajemi_html(url):
     return html_strani
 #pridobi text, v katerem dalje iščemo imena championov
 
-def pridobi_imena(besedilo):
+def pridobi_imena(url):
+    besedilo = zajemi_html(url)
     vzorec = r'''/></div><div class="champion-name">(?P<champion>.*?)</div></a><a class="champion-link" href="/lol/champions/'''
     seznam_championov = re.findall(vzorec, besedilo) 
-    return seznam_championov.replace("&#x27;", "").lower()  #popravi napačne zapise imen
+    return seznam_championov
 #iz html-ja vrne seznam vseh championov, čez katere bomo iteratali
 
-def pridobi_html_vsakega_championa(seznam):
+def pridobi_html_vsakega_championa(url):
+    seznam = pridobi_imena(url)
     os.mkdir("podatki")
     for champion in seznam:
         with open(f"podatki/{champion}.html", "w", encoding="utf-8") as dat:
             dat.write(zajemi_html(url_glavne_strani + f"/{champion}/build"))
-
-#pridobi_html_vsakega_championa(pridobi_imena(zajemi_html(url_glavne_strani)))
+#ustvari html datoteke, ki jih bomo potrebovali za vsakega championa
 
 def pridobi_podrobnosti(champion_html_text):
     with open(champion_html_text, "r", encoding="utf-8") as dat:
         tekstovna = dat.read()
         vzorec = r'''<div class="value">(?P<win_rate>.*?)%</div><div class="label">Win Rate</div></div><div class="overall-rank"><div class="value">(?P<rank>.*?)</div><div class="label">Rank</div></div><div class="pick-rate"><div class="value">(?P<pick_rate>.*?)%</div><div class="label">Pick Rate</div></div><div class="ban-rate"><div class="value">(?P<ban_rate>.*?)%</div><div class="label">Ban Rate</div></div><div class="matches"><div class="value">(?P<matches>.*?)</div><div class="label">Matches</div>'''
-    
+            
     return list(re.findall(vzorec, tekstovna)[0])
 #vrne seznam, ki ima winrate, rank, pickrate,banrate
+
+def pridobi_podrobnosti_vsakega():
+    slovar = {}
+    for champion in pridobi_imena(url_glavne_strani):
+        slovar[champion] = pridobi_podrobnosti(f"podatki/{champion}.html")
+    return slovar
 
 
 
