19056beefc8d490fc5efeaeea896c5f6966a5144
lovroklinc <lovro.klinc@gmail.com>
2024-08-20 14:21:19 +0200

    Popravil delovanje funkcij in dodal funkcijo, ki zajame še "role".

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import os
+from pomozne_funkcije import *
 
 url_glavne_strani = "https://u.gg/lol/champions"
 
@@ -18,7 +19,7 @@ def pridobi_imena(url):
     besedilo = zajemi_html(url)
     vzorec = r'''/></div><div class="champion-name">(?P<champion>.*?)</div></a><a class="champion-link" href="/lol/champions/'''
     seznam_championov = re.findall(vzorec, besedilo) 
-    return seznam_championov
+    return popravi_imena_championov(seznam_championov)
 #iz html-ja vrne seznam vseh championov, čez katere bomo iteratali
 
 def pridobi_html_vsakega_championa(url):
@@ -33,15 +34,24 @@ def pridobi_podrobnosti(champion_html_text):
     with open(champion_html_text, "r", encoding="utf-8") as dat:
         tekstovna = dat.read()
         vzorec = r'''<div class="value">(?P<win_rate>.*?)%</div><div class="label">Win Rate</div></div><div class="overall-rank"><div class="value">(?P<rank>.*?)</div><div class="label">Rank</div></div><div class="pick-rate"><div class="value">(?P<pick_rate>.*?)%</div><div class="label">Pick Rate</div></div><div class="ban-rate"><div class="value">(?P<ban_rate>.*?)%</div><div class="label">Ban Rate</div></div><div class="matches"><div class="value">(?P<matches>.*?)</div><div class="label">Matches</div>'''
-            
-    return list(re.findall(vzorec, tekstovna)[0])
+
+    return popravi_podrobne_podatke(list(re.findall(vzorec, tekstovna)[0]))
 #vrne seznam, ki ima winrate, rank, pickrate,banrate
 
+def pridobi_role(champion_html_text):
+    with open(champion_html_text, "r", encoding="utf-8") as dat:
+        tekstovna = dat.read()
+        vzorec = r'''</svg><div style="margin-left:12px">(?P<role>.*?)</div></div></div><style data-emotion-css="62g3xt-dummyInput">'''
+    return [re.findall(vzorec, tekstovna)[0]]
+
 def pridobi_podrobnosti_vsakega():
+    pridobi_html_vsakega_championa(url_glavne_strani)
     slovar = {}
     for champion in pridobi_imena(url_glavne_strani):
-        slovar[champion] = pridobi_podrobnosti(f"podatki/{champion}.html")
+        slovar[champion] = pridobi_podrobnosti(f"podatki/{champion}.html") + pridobi_role(f"podatki/{champion}.html")
     return slovar
+
+pridobi_podrobnosti_vsakega()
 
 
 
