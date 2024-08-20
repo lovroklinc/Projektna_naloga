ad3496413294a4b3a8dbc06b537323614317ed4d
lovroklinc <lovro.klinc@gmail.com>
2024-08-20 14:20:24 +0200

    Popravil manjše probleme pomožnih funkcij.

--- pomozne_funkcije.py
+++ pomozne_funkcije.py
@@ -1,12 +1,16 @@
 def popravi_imena_championov(seznam):
     popravljen = []
     for champion in seznam:
-        popravljen += champion.replace("&#x27;", "").replace(" ", "").replace(".", "")
+        popravljen.append(champion.replace("&#x27;", "").replace(".", "").replace("Nunu &amp; Willump", "Nunu").replace("Renata Glasc", "Renata"))
     return popravljen
+#popravi problematične zapise imen championov
+#Renata Glasc je na strani spravljena zgolj kot Renata, zato bomo ta zapis uporabili tudi mi
 
 def popravi_podrobne_podatke(seznam):
     popravljen = []
+    del seznam[1]
     for podatek in seznam[:3]:
-        popravljen += float(podatek)
-    popravljen += int(seznam[3].replace(",", ""))
+        popravljen += [float(podatek)]
+    popravljen += [int(seznam[3].replace(",", ""))]
     return popravljen
+#odstrani "rank", ki nas verjetno ne zanima in popravi tip podatkov
